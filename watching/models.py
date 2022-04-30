42956022c96516506c774d31be69412e8a0cb4ec
Mefodii <mefodii04@gmail.com>
2022-04-30 14:51:09 +0300

    clean-up

--- watching/models.py
+++ watching/models.py
@@ -3,30 +3,30 @@ from django.db.models import Q
 
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-VIDEO_TYPE_ANIME = "Anime"
-VIDEO_TYPE_MOVIE = "Movie"
-VIDEO_TYPE_SERIAL = "Serial"
+WATCHIO_TYPE_ANIME = "Anime"
+WATCHIO_TYPE_MOVIE = "Movie"
+WATCHIO_TYPE_SERIAL = "Serial"
 
-VIDEO_STATUS_DROPPED = "Dropped"
-VIDEO_STATUS_PLANNED = "Planned"
-VIDEO_STATUS_IGNORED = "Ignored"
-VIDEO_STATUS_WATCHING = "Watching"
-VIDEO_STATUS_FINISHED = "Finished"
+WATCHIO_STATUS_DROPPED = "Dropped"
+WATCHIO_STATUS_PLANNED = "Planned"
+WATCHIO_STATUS_IGNORED = "Ignored"
+WATCHIO_STATUS_WATCHING = "Watching"
+WATCHIO_STATUS_FINISHED = "Finished"
 
 ALIAS_SEPARATOR = ">;<"
 
-VIDEO_TYPE_CHOICES = (
-    (VIDEO_TYPE_ANIME, VIDEO_TYPE_ANIME),
-    (VIDEO_TYPE_MOVIE, VIDEO_TYPE_MOVIE),
-    (VIDEO_TYPE_SERIAL, VIDEO_TYPE_SERIAL),
+WATCHIO_TYPE_CHOICES = (
+    (WATCHIO_TYPE_ANIME, WATCHIO_TYPE_ANIME),
+    (WATCHIO_TYPE_MOVIE, WATCHIO_TYPE_MOVIE),
+    (WATCHIO_TYPE_SERIAL, WATCHIO_TYPE_SERIAL),
 )
 
-VIDEO_STATUS_CHOICES = (
-    (VIDEO_STATUS_DROPPED, VIDEO_STATUS_DROPPED),
-    (VIDEO_STATUS_PLANNED, VIDEO_STATUS_PLANNED),
-    (VIDEO_STATUS_IGNORED, VIDEO_STATUS_IGNORED),
-    (VIDEO_STATUS_WATCHING, VIDEO_STATUS_WATCHING),
-    (VIDEO_STATUS_FINISHED, VIDEO_STATUS_FINISHED),
+WATCHIO_STATUS_CHOICES = (
+    (WATCHIO_STATUS_DROPPED, WATCHIO_STATUS_DROPPED),
+    (WATCHIO_STATUS_PLANNED, WATCHIO_STATUS_PLANNED),
+    (WATCHIO_STATUS_IGNORED, WATCHIO_STATUS_IGNORED),
+    (WATCHIO_STATUS_WATCHING, WATCHIO_STATUS_WATCHING),
+    (WATCHIO_STATUS_FINISHED, WATCHIO_STATUS_FINISHED),
 )
 
 
@@ -42,12 +42,12 @@ class Group(models.Model):
     name = models.CharField(max_length=200)
     # Alias name for video. Separated by string ALIAS_SEPARATOR
     alias = models.CharField(max_length=1000, blank=True)
-    type = models.CharField(max_length=50, choices=VIDEO_TYPE_CHOICES)
+    type = models.CharField(max_length=50, choices=WATCHIO_TYPE_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     check_date = models.DateField(blank=True, null=True)
     single = models.BooleanField(default=False, blank=True)
-    status = models.CharField(max_length=50, choices=VIDEO_STATUS_CHOICES, blank=True, null=True)
+    status = models.CharField(max_length=50, choices=WATCHIO_STATUS_CHOICES, blank=True, null=True)
 
     objects = GroupQuerySet.as_manager()
 
@@ -77,8 +77,8 @@ class Video(models.Model):
     # Alias name for video. Separated by string ALIAS_SEPARATOR
     alias = models.CharField(max_length=1000, blank=True)
     year = models.IntegerField(null=True, blank=True)
-    type = models.CharField(max_length=50, choices=VIDEO_TYPE_CHOICES)
-    status = models.CharField(max_length=50, choices=VIDEO_STATUS_CHOICES)
+    type = models.CharField(max_length=50, choices=WATCHIO_TYPE_CHOICES)
+    status = models.CharField(max_length=50, choices=WATCHIO_STATUS_CHOICES)
     order = models.IntegerField(default=1)
     episodes = models.IntegerField(default=1)
     rating = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(10)])
--- watching/views.py
+++ watching/views.py
@@ -1,10 +1,10 @@
 from django.http import JsonResponse
-from .models import VIDEO_STATUS_CHOICES, VIDEO_TYPE_CHOICES, ALIAS_SEPARATOR
+from .models import WATCHIO_STATUS_CHOICES, WATCHIO_TYPE_CHOICES
 
 
 def get_info(request):
     info = {
-        "statusTypes": [choice[0] for choice in VIDEO_STATUS_CHOICES],
-        "videoTypes": dict((x.lower(), y) for x, y in VIDEO_TYPE_CHOICES),
+        "statusTypes": [choice[0] for choice in WATCHIO_STATUS_CHOICES],
+        "watchioTypes": dict((x.lower(), y) for x, y in WATCHIO_TYPE_CHOICES),
     }
     return JsonResponse(info)

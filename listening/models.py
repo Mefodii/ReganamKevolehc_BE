5bc10f03ed4c5d7f5eba2fe6e88120069ec13421
Mefodii <mefodii04@gmail.com>
2022-09-15 22:07:49 +0300

    Alias field to Artist & Track

--- listening/models.py
+++ listening/models.py
@@ -2,14 +2,26 @@ from datetime import datetime
 
 from django.db import models
 
+ALIAS_SEPARATOR = ">{;}<"
+
 
 class Artist(models.Model):
     name = models.CharField(max_length=300)
+    alias = models.CharField(max_length=1000, blank=True)
     monitoring = models.BooleanField(default=False)
     check_date = models.DateTimeField(default=datetime(2001, 1, 1))
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def get_aliases(self):
+        if self.alias:
+            return self.alias.split(ALIAS_SEPARATOR)
+        return []
+
+    @staticmethod
+    def build_alias(aliases):
+        return ALIAS_SEPARATOR.join(aliases)
 
 
 class Album(models.Model):
@@ -23,8 +35,18 @@ class Album(models.Model):
 
 class Track(models.Model):
     title = models.CharField(max_length=300)
+    alias = models.CharField(max_length=1000, blank=True)
     in_playlist = models.BooleanField(default=False)
     artist = models.ForeignKey(Artist, related_name="track", on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def get_aliases(self):
+        if self.alias:
+            return self.alias.split(ALIAS_SEPARATOR)
+        return []
+
+    @staticmethod
+    def build_alias(aliases):
+        return ALIAS_SEPARATOR.join(aliases)
